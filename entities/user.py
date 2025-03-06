01abc34a757be252f64903cccfb6cbc9fe50a0ea
Renan-Silva235 <renan.rubio94@gmail.com>
2025-03-06 10:51:03 -0300

    commit

---
+++ entities/user.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, name, last_name, cpf, password):
+        self.name = name
+        self.last_name = last_name
+        self.cpf = cpf
+        self.password = password
+
+    def create_user(self):
+        
+
