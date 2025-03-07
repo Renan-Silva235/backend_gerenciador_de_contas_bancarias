c4bbde3440a318a8d6dddeb58ff113ac768ee87b
Renan-Silva235 <renan.rubio94@gmail.com>
2025-03-07 12:06:19 -0300

    criando banco de dados e criando o módulo user

--- entities/user.py
+++ entities/user.py
@@ -1,10 +1,57 @@
-class User:
-    def __init__(self, name, last_name, cpf, password):
-        self.name = name
-        self.last_name = last_name
-        self.cpf = cpf
-        self.password = password
+from model.databases import RegisterUser, engine
+from sqlmodel import Session
+import bcrypt
 
-    def create_user(self):
+
+
+class User:
+    def register_user(self, name, cpf, email, password):
+
+        def set_password(password):
+            #criptografa a senha
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            return hashed_password.decode('utf-8')
+        
+        with Session(engine) as session:
+            user = RegisterUser(name=name, cpf=cpf, email=email, password=set_password(password))
+            session.add(user)
+            session.commit()
+
+
+
+    def login(self, cpf, password):
+
+        #verificar cpf
+        with Session(engine) as session:
+
+            user = session.query(RegisterUser).filter(RegisterUser.cpf == cpf).first()
+
+            if user is None:
+                print('cpf inválido')
+                return False
+            
+
+        
+    
+            #verificando senha
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                print('login bem-sucedido')
+                return True
+
+            else:
+                print('senha inválida')
+                return False
+                
+          
+
+     
+
+            
+
+
+
+        
+
+        
         
 
