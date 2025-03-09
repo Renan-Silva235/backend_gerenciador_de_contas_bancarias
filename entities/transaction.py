022b3c959bb7118f56106cd0aa4f307fbcfac514
Renan-Silva235 <renan.rubio94@gmail.com>
2025-03-08 22:23:25 -0300

    add method transfer

--- entities/transaction.py
+++ entities/transaction.py
@@ -33,8 +33,45 @@ class Transaction:
                 withdraw.balance -= value
                 session.add(withdraw)
                 session.commit()
-               
-  
+
+
+    def transfer(self, user:RegisterUser, cpf, value):
+
+
+
+        
+        def query_target_user(cpf:str, value):
+            with Session(engine) as target_session:
+                target_user = target_session.query(RegisterUser).filter(RegisterUser.cpf == cpf).first()
+
+                if target_user:
+                    target_transaction = target_session.query(TransactionModel).filter(TransactionModel.user_id == target_user.user_id).first()
+
+                    if target_transaction:
+                        target_transaction.balance += value
+                        target_session.add(target_transaction)
+                        target_session.commit()
+                        
+            return True
+                
+
+
+
+        with Session(engine) as session:
+            query_balance = session.query(TransactionModel).filter(TransactionModel.user_id == user.user_id).first()
+
+            if query_balance:
+                if query_balance.balance >= value:
+                    send_money = query_target_user(cpf, value)
+
+                    if send_money is True:
+                        query_balance.balance -= value
+                    
+                    session.add(query_balance)
+                    session.commit()
+                    return 'Transferência concluída'
+                else:
+                    return 'Saldo insuficiente'
             
             
 
