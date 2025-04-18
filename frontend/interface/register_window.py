12b83211ab3de64e33bd3fe8dea40d105d988343
Renan-Silva235 <renan.rubio95@gmail.com>
2025-04-18 00:27:29 -0300

    tela de cadastro do usuário

---
+++ frontend/interface/register_window.py
@@ -0,0 +1,75 @@
+from .interface import BaseWindow
+from .menu import MainMenu
+from tkinter import ttk, messagebox
+from backend.entities.user import User
+
+class RegisterWindow(BaseWindow):
+    def __init__(self, parent):
+        super().__init__(title='Cadastro')
+        self.parent = parent
+        self.user = User()
+        self.create_widgets()
+
+    
+    
+    def create_widgets(self):
+        
+        self.label = ttk.Label(self, text='Cadastro de Usuário', font=('Arial', 30), background='#C0C0C0') 
+        self.label.pack(pady=20)
+        
+        
+        self.label2 = ttk.Label(self, text='Preencha os campos abaixo para criar uma conta.', font=("Arial", 16), background='#C0C0C0')
+        self.label2.pack(pady=20)
+        
+        
+        
+        self.name_label = ttk.Label(self, text='Nome:', font=('Arial', 16), background='#C0C0C0')
+        self.name_label.pack(pady=10)
+        self.name_entry = ttk.Entry(self, font=('Arial', 16))
+        self.name_entry.pack(pady=10)
+        
+        self.cpf_label = ttk.Label(self, text='CPF:', font=('Arial', 16), background='#C0C0C0')
+        self.cpf_label.pack(pady=10)
+        self.cpf_entry = ttk.Entry(self, font=('Arial', 16))
+        self.cpf_entry.pack(pady=10)
+
+        self.email_label = ttk.Label(self, text='Email:', font=('Arial', 16), background='#C0C0C0')
+        self.email_label.pack(pady=10)
+        self.email_entry = ttk.Entry(self, font=('Arial', 16))
+        self.email_entry.pack(pady=10)
+
+        self.password_label = ttk.Label(self, text='Senha:', font=('Arial', 16), background='#C0C0C0')
+        self.password_label.pack(pady=10)
+        self.password_entry = ttk.Entry(self, font=('Arial', 16))
+        self.password_entry.pack(pady=10)
+
+        self.button = ttk.Button(self, text='Cadatrar', command=self.register_user)
+        self.button.pack(pady=10)
+
+    def register_user(self):
+        name = self.name_entry.get()
+        cpf = self.cpf_entry.get()
+        email = self.email_entry.get()
+        password = self.password_entry.get()
+
+        if not name or not cpf or not email or not password:
+            messagebox.showerror('Erro', 'Por favor, preencha todos os campos.')
+            return
+        
+        register = self.user.register_user(name, cpf, email, password)
+
+        if register == 'E-mail-inválido':
+            messagebox.showerror('Erro', 'E-mail inválido.')
+        elif register == 'senha inválida':
+            messagebox.showerror('Erro', 'Senha inválida.')
+        elif register == 'cpf inválido':
+            messagebox.showerror('Erro', 'CPF inválido.')
+        elif register == 'CPF já cadastrado':
+            messagebox.showerror('Erro', 'Esse CPF já está cadastrado.')
+        else:
+            messagebox.showinfo('Sucesso', 'Usuário cadastrado com sucesso!')
+            self.destroy()
+            
+            if self.parent:
+                self.parent.deiconify()
+
